1f444fd5587c44ba583154cf76a1566dfb6ace39
Isaiah Peng <issaria@gmail.com>
2015-07-03 23:25:34 +0200

    add churn calculation by author

--- churn_test.go
+++ churn_test.go
@@ -20,12 +20,25 @@ var (
 func TestAbsolutesTrend(t *testing.T) {
 	churns := AbsoluteTrends(simple)
 	if len(churns) != 2 {
-		t.Fatalf("expected 2 churns, get %d", len(churns))
+		t.Fatalf("expected 2 churns, got %d", len(churns))
 	}
 	if churns[0].Date != "2015-07-01" {
-		t.Errorf("churns not sorted")
+		t.Error("churns are not sorted")
 	}
 	if churns[0].Added != 10 || churns[0].Deleted != 1 {
+		t.Errorf("wrong Added %d, Deleted %d", churns[0].Added, churns[0].Deleted)
+	}
+}
+
+func TestChurnByAuthor(t *testing.T) {
+	churns := ByAuthor(simple)
+	if len(churns) != 2 {
+		t.Fatalf("expected 2 churns, got %d", len(churns))
+	}
+	if churns[0].Author != "IS" {
+		t.Error("churns are not sorted")
+	}
+	if churns[0].Added != 12 || churns[0].Deleted != 1 {
 		t.Errorf("wrong Added %d, Deleted %d", churns[0].Added, churns[0].Deleted)
 	}
 }
